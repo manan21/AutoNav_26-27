7bd5feaed9bb2068130c95ce6f98296ded1de7cd
Tamir <tamirlan@gmail.com>
2026-01-29 15:33:43 -0500

    Updated ros-args in run-lines.sh script

--- isaac_ros-dev/config/run-lines.sh
+++ isaac_ros-dev/config/run-lines.sh
@@ -1,1 +1,1 @@
-ros2 run line_detection line_detector --ros-args -p camera_topic:="/zed/camera/image_raw" -p depth_camera_topic:="/zed/camera/depth/image_raw" -p camera_info_topic:="/zed/camera/camera_info"
+ros2 run line_detection line_detector --ros-args -p camera_topic:="/zed/zed_node/rgb/color/rect/image" -p depth_camera_topic:="/zed/zed_node/depth/depth_registered" -p camera_info_topic:="/zed/zed_node/rgb/color/rect/camera_info"
