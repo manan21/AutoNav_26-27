ef23f9834db3646e2a041e2fb81a2d0bffe1319f
Tamir <tamirlan@gmail.com>
2026-03-20 16:28:30 -0400

    FIxing local costmap tilt

--- isaac_ros-dev/config/run-lines.sh
+++ isaac_ros-dev/config/run-lines.sh
@@ -3,4 +3,5 @@ ros2 run line_detection line_detector --ros-args \
     -p depth_camera_topic:="/zed/zed_node/depth/depth_registered" \
     -p camera_info_topic:="/zed/zed_node/rgb/color/rect/camera_info" \
     -p line_points_topic:="/line_detection/line_points" \
+    -p target_frame:="map" \
     -p enable_timer:=true
--- isaac_ros-dev/config/run-zed.sh
+++ isaac_ros-dev/config/run-zed.sh
@@ -2,4 +2,6 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)
 ros2 launch zed_wrapper zed_camera.launch.py \
   camera_model:='zed2i' \
+  publish_tf:=false \
+  publish_map_tf:=false \
   ros_params_override_path:="$SCRIPT_DIR/zed_static_override.yaml"
