f5183226c011b42299d285443fefcc6a55379292
Tamir <tamirlan@gmail.com>
2026-03-20 15:55:03 -0400

    Made camera static relative to the static frames

--- isaac_ros-dev/config/run-zed.sh
+++ isaac_ros-dev/config/run-zed.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-ros2 launch zed_wrapper zed_camera.launch.py camera_model:='zed2i' 
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)
+ros2 launch zed_wrapper zed_camera.launch.py \
+  camera_model:='zed2i' \
+  ros_params_override_path:="$SCRIPT_DIR/zed_static_override.yaml"
